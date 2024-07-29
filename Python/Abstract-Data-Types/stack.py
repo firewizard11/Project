615725389c9c1ec4351b482649990579e302d823
firewizard11 <seriousemail178@gmail.com>
2024-07-29 11:56:39 +1000

    update stack.py

--- Python/Abstract-Data-Types/stack.py
+++ Python/Abstract-Data-Types/stack.py
@@ -1,17 +1,22 @@
+from abc import ABC, abstractmethod
 from typing import TypeVar, Generic
+from referential_array import ArrayR
 T = TypeVar('T')
 
-class Stack(Generic[T]):
+class Stack(ABC, Generic[T]):
 
     def __init__(self) -> None:
         self.length = 0
 
+    @abstractmethod
     def push(self, item: T) -> None:
         pass
 
+    @abstractmethod
     def pop(self) -> T:
         pass
 
+    @abstractmethod
     def peek(self) -> T:
         pass
 
@@ -21,8 +26,41 @@ class Stack(Generic[T]):
     def is_empty(self) -> bool:
         return len(self) == 0
     
+    @abstractmethod
     def is_full(self) -> bool:
         pass
 
     def clear(self) -> None:
         self.length = 0
+
+
+class ArrayStack(Stack[T]):
+    
+    MIN_CAPACITY = 1
+
+    def __init__(self, max_capacity: int) -> None:
+        Stack.__init__(self)
+        self.array = ArrayR(max(MIN_CAPACITY, max_capacity))
+
+    def is_full(self) -> bool:
+        return len(self) == len(self.array)
+    
+    def push(self, item: T) -> None:
+        if self.is_full():
+            raise Exception("Stack is full")
+        
+        self.array[len(self)] = item
+        self.length += 1
+
+    def pop(self) -> T:
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        
+        self.length -= 1
+        return self.array[self.length]
+    
+    def peek(self) -> T:
+        if self.is_empty():
+            raise Exception("Stack is empty")
+        
+        return self.array[self.length-1]
