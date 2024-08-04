3d35a22276da31f127210a4043f864b3b30a660e
firewizard11 <seriousemail178@gmail.com>
2024-08-04 20:05:36 +1000

    update challenge1.py

---
+++ Python/dailyprogrammer/challenge1/challenge1.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    name = input('Please Enter your Name: ')
+    age = input('Please Enter your Age: ')
+    username = input('Please Enter your Reddit Username: ')
+    print(f'Your name is {name}, you are {age} years old, and your username is {username}')
