f1a0a355de77572223cae72b7e7d0fccd2ebd2e0
firewizard11 <seriousemail178@gmail.com>
2024-08-05 22:33:56 +1000

    update challenge5.py

--- Python/dailyprogrammer/challenge5/challenge5.py
+++ Python/dailyprogrammer/challenge5/challenge5.py
@@ -1,8 +1,18 @@
+def read_file(file_name):
+    with open(file_name, 'r') as file_read:
+        username = file_read.readline().strip()
+        password = file_read.readline()
+    return (username, password)
+
 if __name__ == '__main__':
-    if input('Please enter your username: ') == 'root':
-        if input('Please enter your password: ') == '123':
+    creds = read_file('secret.txt')
+
+    if input('Please enter your username: ') == creds[0]:
+        if input('Please enter your password: ') == creds[1]:
             print('You are authenticated!!!!')
         else:
             print('Incorrect Password!')
     else:
         print('Incorrect Username!')
+
+    input('Click Enter to close: ')
