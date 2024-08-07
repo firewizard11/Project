c7ba2595988ecb912d489ed41da82f09f2fb6b37
firewizard11 <seriousemail178@gmail.com>
2024-08-07 22:22:19 +1000

    create challenge7

---
+++ Python/dailyprogrammer/challenge7/challenge7.py
@@ -0,0 +1,45 @@
+map = {
+    'a':'.-',
+    'b':'-...',
+    'c':'-.-.',
+    'd':'-..',
+    'e':'.',
+    'f':'..-.',
+    'g':'--.',
+    'h':'....',
+    'i':'..',
+    'j':'.---',
+    'k':'-.-',
+    'l':'.-..',
+    'm':'--',
+    'n':'-.',
+    'o':'---',
+    'p':'.--.',
+    'q':'--.-',
+    'r':'.-.',
+    's':'...',
+    't':'-',
+    'u':'..-',
+    'v':'...-',
+    'w':'.--',
+    'x':'-..-',
+    'y':'-.--',
+    'z':'--..'
+}
+
+def translate_to_morse(text: str) -> str:
+    morse = ''
+    
+    for c in text.lower():
+        if c == ' ':
+            morse += ' / '
+        else:
+            morse += map[c]
+    
+    return morse
+
+if __name__ == '__main__':
+    test_text = 'bobby'
+    text_to_morse = translate_to_morse(test_text)
+
+    print(text_to_morse)
